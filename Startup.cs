0092bab5dbd3ce2b920a04df9907d1acae92e796
David Gristwood <davidgristwood@hotmail.com>
2021-03-17 16:11:57 +0000

    first version to work locally

---
+++ Startup.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.ML;
+using romcom;
+
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace romcom
+{
+    public class Startup : FunctionsStartup
+    {
+        private  string _environment;
+        private  string _modelPath;
+
+        public Startup()
+        {
+            _environment = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT");
+            Console.WriteLine($"_environment {_environment}");
+            Console.WriteLine($"CurrentDirectory {Environment.CurrentDirectory}");
+
+            if (_environment == "Development")
+            {
+                _modelPath = Path.Combine(@"..\..\..\MLModels", "onnxmlmodel.zip");
+            }
+            else
+            {
+                string deploymentPath = @"D:\home\site\wwwroot\";
+                _modelPath = Path.Combine(deploymentPath, "MLModels", "onnxmlmodel.zip");
+            }
+            Console.WriteLine($"_modelPath {_modelPath}");
+        }
+        
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // AddPredictionEnginePool<Input,Output>()
+            builder.Services.AddPredictionEnginePool<OnnxInput, OnnxOutput>()
+                    .FromFile(modelName: "RomComModel", filePath: _modelPath, watchForChanges: true);
+                    //.FromUri(modelName: "RomComModel", "https://github.com/davidgristwood/romcomv2/blob/main/MLModels/onnxmlmodel.zip");
+        }
+    }
+}
